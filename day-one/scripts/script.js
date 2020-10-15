192f1727f5f701e8822d306268df48ced88a56fb
ArupDuttaBappy <ad.bappy10@gmail.com>
2020-10-15 21:07:19 +0600

    Update script.js

--- day-one/scripts/script.js
+++ day-one/scripts/script.js
@@ -1,14 +1,16 @@
 function play_sound(e){
   const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const pressed_key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  pressed_key.classList.add("playing");//key animation
   if(!audio) return;
-  pressed_key.classList.add("playing");
-  // settimeout() works same as
+  // settimeout() works same as transitionend event
   //setTimeout(function(){ pressed_key.classList.remove("playing"); }, 70);
   audio.currentTime=0;//restarts audio on every keystroke immediately
   audio.play();
 }
+
 window.addEventListener("keydown",play_sound);
+
 function remove_transition(e){
   this.classList.remove("playing");
 }
